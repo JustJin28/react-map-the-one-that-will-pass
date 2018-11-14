763c3dbf90a5198de357396351464de50106a859
JustJin28 <39979913+JustJin28@users.noreply.github.com>
2018-11-14 17:38:47 -0500

    save

--- src/Components/GMap.js
+++ src/Components/GMap.js
@@ -1,127 +1,127 @@
 import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper
+} from 'google-maps-react';
 
 var  AllPlaces = [
-      {
-        "name" : "Pizza",
-        "lat": "40.7589",
-        "lng":"-73.9851",
-      },
+{
+"name" : "Pizza",
+"lat": "40.7589",
+"lng":"-73.9851",
+},
 
-      {
-        "name" : "Cookies",
-        "lat": "40.7690",
-        "lng":"-73.9952",
-      }
-    ]
+{
+"name" : "Cookies",
+"lat": "40.7690",
+"lng":"-73.9952",
+},
+{
+"name" : "Bagels",
+"lat": "40.7489",
+"lng":"-73.9751",
+}
+]
 
 class MapContainer extends Component {
-  state = {
+state = {
+showingInfoWindow: false,
+activeMarker: {},
+selectedPlace: {},
+query:'',
+filteredPlaces: []
+};
+
+markers = []
+
+
+onMarkerClick = (props, marker, e) => {
+this.setState({
+  selectedPlace: props,
+  activeMarker: marker,
+  showingInfoWindow: true
+ });
+}
+
+onLiClick = (i) =>{
+this.setState({
+    showingInfoWindow: true,
+    activeMarker: this.markers[i],
+    selectedPlace: AllPlaces[i]
+})
+}
+
+
+onMapClicked = (props) => {
+if (this.state.showingInfoWindow) {
+  this.setState({
     showingInfoWindow: false,
-    activeMarker: {},
-    selectedPlace: {},
-    query:'',
-  };
+    activeMarker: null
+  })
+}
+}
 
 
+CreateInputField = () => (
+<input
+  placeholder = "Search Nearby Places"
+  onChange={(event) => this.setState({filteredPlaces: AllPlaces.filter(place => !place.name.startsWith(event.target.value))})}
+/>
+)
 
-  onMarkerClick = (props, marker, e) => {
-    console.log(marker);
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
-  }
-
-  onLiClick = (markers) =>{
-    //AllPlaces.foreach(
-      //if(markers.target.innerHTML === )
-    //google.maps.event.trigger(marker,'click'))
-  }
-
-
-  onMapClicked = (props) => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      })
-    }
-  }
-
-  updateQuery = (query) => {
-    this.setState({ query }, () => {
-
-    });
-  }
-
-  CreateInputField = () => {
-    const { query } = this.state;
-
-    return <input
-    value = {query}
-    onChange={event => this.updateQuery(event.target.value)}
-    placeholder = "Search Nearby Places"
-    />
-  }
-
-  findPlaces = () => {
-    return(
-      <ol className='Places'>
-        {AllPlaces.map((arrayItem, index)=>
-          <li
-          key = {index}
-          className='Place'
-          //onClick={() => {this.onLiClick}}
-          >{arrayItem.name}</li>
-        )}
-      </ol>
-    )
-  };
-
-  render() {
-    return (
-      <div className = 'map-container' style={{marginleft:'250px'}}>
-        <div>
-          <div className = 'sideMenu'>
-            <div className = 'List'>
-              <h1 className = 'title'> Places to Eat </h1>
-                {this.CreateInputField()}
-            </div>
-            <div className = 'PlaceList'>
-              {this.findPlaces()}
-            </div>
-          </div>
+render() {
+return (
+  <div className = 'map-container' style=
+{{marginleft:'250px'}}>
+    <div>
+      <div className = 'sideMenu'>
+        <div className = 'List'>
+          <h1 className = 'title'> Places to Eat
+</h1>
+            {this.CreateInputField()}
         </div>
-        <Map google={this.props.google} zoom={14}
-          initialCenter = {{lat:40.7589, lng:-73.9851}}
-          onClick={this.onMapClicked}>
-          <Marker
-            onClick={this.onMarkerClick}
-            title = {AllPlaces[0].name}
-            name={AllPlaces[0].name}
-            position = {{lat:AllPlaces[0].lat,lng:AllPlaces[0].lng}}/>
-          <Marker
-            onClick={this.onMarkerClick}
-            title = {AllPlaces[1].name}
-            name={AllPlaces[1].name}
-            position = {{lat:AllPlaces[1].lat,lng:AllPlaces[1].lng}}/>
-          <InfoWindow
-            onOpen={this.windowHasOpened}
-            onClose={this.windowHasClosed}
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-          </InfoWindow>
-        </Map>
+        <div className = 'PlaceList'>
+          <ol className='Places'>
+            {AllPlaces.map((arrayItem, index)=>
+            !this.state.filteredPlaces.includes(arrayItem) &&
+              <li
+              key = {index}
+              className='Place'
+              onClick={() => {this.onLiClick(index)}}
+              >{arrayItem.name}</li>
+            )}
+          </ol>
+        </div>
       </div>
-    );
-  }
+    </div>
+    <Map google={this.props.google} zoom={14}
+      initialCenter = {{lat:40.7589, lng:-73.9851}}
+      onClick={this.onMapClicked}>
+      {AllPlaces.map((marker, i) =>
+        !this.state.filteredPlaces.includes(marker) &&
+          <Marker
+          ref={(e) => {if (e) this.markers[i] =
+ e.marker}}
+          onClick={this.onMarkerClick}
+          title = {marker.name}
+          name={marker.name}
+          position =
+{{lat:marker.lat,lng:marker.lng}}
+          />
+      )}
+      <InfoWindow
+        onOpen={this.windowHasOpened}
+        onClose={this.windowHasClosed}
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}>
+        <div>
+          <h1>{this.state.selectedPlace.name}</h1>
+        </div>
+      </InfoWindow>
+    </Map>
+  </div>
+ );
+ }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyC21SntdNn1vCb5VOAujCPIM7a9p5XkvRs'
+apiKey: 'AIzaSyD2BmXOGrCZQiAg5HJ6hU70BXc5v6Osr6M'
 })(MapContainer)
